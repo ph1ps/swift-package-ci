ccc94a267f48d7d477c242853b0bfccfaa2c5e3e
Philipp Gabriel <ph1psdev@gmail.com>
2025-03-23 18:09:30 +0100

    initialc ommit

--- Package.swift
+++ Package.swift
@@ -1,14 +1,18 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "SwiftPackageIndexCI",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "SwiftPackageIndexCI"),
-    ]
+  name: "SwiftPackageIndexCI",
+  platforms: [.macOS(.v15)],
+  dependencies: [
+    .package(url: "https://github.com/iCharlesHu/Subprocess", branch: "main")
+  ],
+  targets: [
+    .executableTarget(
+      name: "SwiftPackageIndexCI",
+      dependencies: [
+        .product(name: "Subprocess", package: "Subprocess")
+      ]
+    ),
+  ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,106 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Subprocess
 
-print("Hello, world!")
+let packages = [
+  "pointfreeco/combine-schedulers",
+  "pointfreeco/sharing-grdb",
+  "pointfreeco/swift-case-paths",
+  "pointfreeco/swift-clocks",
+  "pointfreeco/swift-concurrency-extras",
+  "pointfreeco/swift-custom-dump",
+  "pointfreeco/swift-dependencies",
+  "pointfreeco/swift-gen",
+  "pointfreeco/swift-html",
+  "pointfreeco/swift-html-vapor",
+  "pointfreeco/swift-identified-collections",
+  "pointfreeco/swift-issue-reporting",
+  "pointfreeco/swift-macro-testing",
+  "pointfreeco/swift-navigation",
+  "pointfreeco/swift-nonempty",
+  "pointfreeco/swift-overture",
+  "pointfreeco/swift-parsing",
+  "pointfreeco/swift-perception",
+  "pointfreeco/swift-prelude",
+  "pointfreeco/swift-sharing",
+  "pointfreeco/swift-snapshot-testing",
+  "pointfreeco/swift-tagged",
+  "pointfreeco/swift-url-routing",
+  "pointfreeco/swift-validated",
+  "pointfreeco/swift-web",
+  "pointfreeco/swiftui-navigation"
+]
+
+struct BuildResult {
+  let package: String
+  let buildTime: ContinuousClock.Duration
+}
+
+struct BuildError: Error {
+  let package: String
+  let base: any Error
+}
+
+struct TerminationStatusError: Error {
+  let status: TerminationStatus
+  let standardError: String?
+}
+
+extension CollectedResult where Error == StringOutput<UTF8> {
+  
+  func verifyTerminationStatus0() throws {
+    if terminationStatus != .exited(0) {
+      throw TerminationStatusError(status: terminationStatus, standardError: standardError)
+    }
+  }
+}
+
+let clock = ContinuousClock()
+
+let duration = await clock.measure {
+  
+  await withTaskGroup(of: Result<BuildResult, BuildError>.self) { taskGroup in
+    
+    var packages = packages
+    
+    func queue() {
+      if !packages.isEmpty {
+        let package = packages.removeFirst()
+        taskGroup.addTask {
+          await build(package)
+        }
+      }
+    }
+    
+    func build(_ package: String) async -> Result<BuildResult, BuildError> {
+      do {
+        let packagePath = "repositories/\(package)"
+        let duration = try await clock.measure {
+          try await run(.name("git"), arguments: ["clone", "--depth=1", "https://github.com/\(package).git", packagePath], output: .discarded, error: .string)
+            .verifyTerminationStatus0()
+          try await run(.name("swift"), arguments: ["build", "--package-path", packagePath, "--cache-path", "cache"], output: .discarded, error: .string)
+            .verifyTerminationStatus0()
+        }
+        return .success(.init(package: package, buildTime: duration))
+      } catch {
+        return .failure(.init(package: package, base: error))
+      }
+    }
+    
+    for _ in 0..<4 {
+      queue()
+    }
+    
+    print("remaining", packages.count)
+    for await result in taskGroup {
+      switch result {
+      case .success(let result):
+        print("build success", result.package, "time", result.buildTime)
+      case .failure(let error):
+        print("build failure", error.package, error.base)
+      }
+      queue()
+      print("remaining", packages.count)
+    }
+  }
+}
+
+print("done in", duration)
