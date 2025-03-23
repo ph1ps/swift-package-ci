d4298005aa1bd5219d91631c21f26a9d1e264b72
Philipp Gabriel <ph1ps@users.noreply.github.com>
2025-03-23 18:41:50 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftPackageIndexCI",
-  platforms: [.macOS(.v15)],
   dependencies: [
     .package(url: "https://github.com/iCharlesHu/Subprocess", branch: "main")
   ],
