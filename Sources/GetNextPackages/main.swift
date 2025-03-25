944d5fca153d6ad6a9a1bd85d8d4cf8c66cd61b7
Philipp Gabriel <ph1psdev@gmail.com>
2025-03-25 19:01:12 +0100

    wip

--- Sources/GetNextPackages/main.swift
+++ Sources/GetNextPackages/main.swift
@@ -6,4 +6,4 @@ let githubOutputPath = ProcessInfo.processInfo.environment["GITHUB_OUTPUT"]!
 let allPackages = try JSONDecoder().decode([String].self, from: Data(contentsOf: Bundle.module.url(forResource: "packages", withExtension: "json")!))
 let selectedPackages = allPackages.shuffled().prefix(10)
 
-_ = try await run(.name("echo"), arguments: [String(decoding: JSONEncoder().encode(Array(selectedPackages)), as: UTF8.self)], output: .fileDescriptor(.open(githubOutputPath, .writeOnly), closeAfterSpawningProcess: true))
+_ = try await run(.name("echo"), arguments: ["packages=\(String(decoding: JSONEncoder().encode(Array(selectedPackages)), as: UTF8.self))"], output: .fileDescriptor(.open(githubOutputPath, .readWrite, options: .append), closeAfterSpawningProcess: true))
