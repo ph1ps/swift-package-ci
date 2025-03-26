15bb150cefb9af97e5b84b4568708600798ca7f6
Philipp Gabriel <ph1ps@users.noreply.github.com>
2025-03-26 09:35:16 +0100

    Update main.swift

--- Sources/GetNextPackages/main.swift
+++ Sources/GetNextPackages/main.swift
@@ -4,6 +4,6 @@ import Subprocess
 let githubOutputPath = ProcessInfo.processInfo.environment["GITHUB_OUTPUT"]!
 
 let allPackages = try JSONDecoder().decode([String].self, from: Data(contentsOf: Bundle.module.url(forResource: "packages", withExtension: "json")!))
-let selectedPackages = allPackages.shuffled().prefix(10)
+let selectedPackages = allPackages.shuffled().prefix(5)
 
 _ = try await run(.name("echo"), arguments: ["packages=\(String(decoding: JSONEncoder().encode(Array(selectedPackages)), as: UTF8.self))"], output: .fileDescriptor(.open(githubOutputPath, .readWrite, options: .append), closeAfterSpawningProcess: true))
